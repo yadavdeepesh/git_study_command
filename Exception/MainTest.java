aefc5fa728e5d5e9db349cb07dcfaceb36d1905f
Deepesh Yadav <yadavdeepesh13@gmail.com>
2026-05-17 15:39:59 +0530

    for study merge conflict

--- Exception/MainTest.java
+++ Exception/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello Welcome to javagit  ");
         System.out.println("merge conflict test");
+        System.out.println("merge conflict test");
     }
 }
